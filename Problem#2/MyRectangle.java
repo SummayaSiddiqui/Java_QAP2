cf7eb303f7ba62bd860cb64acc9d63461d02f41d
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-10-07 12:26:52 -0230

    Updated MyRectangle.java and TestMyRectangle.java

--- Problem#2/MyRectangle.java
+++ Problem#2/MyRectangle.java
@@ -1,3 +1,50 @@
 public class MyRectangle {
-    
+    private MyPoint topLeft;
+    private MyPoint bottomRight;
+
+    // Constructors
+    public MyRectangle(int x1, int y1, int x2, int y2) {
+        this.topLeft = new MyPoint(x1, y1);
+        this.bottomRight = new MyPoint(x2, y2);
+    }
+
+    public MyRectangle(MyPoint topLeft, MyPoint bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+    }
+
+    // Getters and Setters
+    public MyPoint getTopLeft() {
+        return this.topLeft;
+    }
+
+    public void setTopLeft(MyPoint topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public MyPoint getBottomRight() {
+        return this.bottomRight;
+    }
+
+    public void setBottomRight(MyPoint bottomRight) {
+        this.bottomRight = bottomRight;
+    }
+
+    // Method to calculate area
+    public int getArea() {
+        int length = bottomRight.getX() - topLeft.getX();
+        int width = topLeft.getY() - bottomRight.getY();
+        return length * width;
+    }
+
+    // Method to calculate perimeter
+    public int getPerimeter() {
+        int length = bottomRight.getX() - topLeft.getX();
+        int width = topLeft.getY() - bottomRight.getY();
+        return 2 * (length + width);
+    }
+
+    public String toString() {
+        return "MyRectangle[topLeft=" + topLeft + ", bottomRight=" + bottomRight + "]";
+    }
 }
--- Problem#2/TestMyRectangle.java
+++ Problem#2/TestMyRectangle.java
@@ -1,3 +1,21 @@
 public class TestMyRectangle {
-    
+    public static void main(String[] args) {
+        // Create two MyPoint objects
+        MyPoint topLeft = new MyPoint(1, 5);
+        MyPoint bottomRight = new MyPoint(4, 1);
+
+        // Create a MyRectangle object
+        System.out.println();
+        MyRectangle rectangle = new MyRectangle(topLeft, bottomRight);
+
+        // Print the rectangle information
+        System.out.println(rectangle);
+
+        System.out.println();
+        // Calculate and print area and perimeter
+        System.out.println("Area of the Rectangle: " + rectangle.getArea() + " sq. units");
+        System.out.println("Perimeter of the Rectangle : " + rectangle.getPerimeter() + " units");
+        System.out.println();
+    }
+
 }
